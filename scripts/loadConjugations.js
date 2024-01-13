13459cb9045a4f738380b469f8f68095ee690dd3
pthomason <paul@trusolutions.com>
2024-01-13 12:47:44 -0500

    Update study to match approach

--- scripts/loadConjugations.js
+++ scripts/loadConjugations.js
@@ -1,26 +1,14 @@
-document.getElementById('pres_yo').innerHTML = verbs[todaysVerb].pres.yo;
-document.getElementById('pres_tu').innerHTML = verbs[todaysVerb].pres.tu;
-document.getElementById('pres_el').innerHTML = verbs[todaysVerb].pres.el;
-document.getElementById('pres_nos').innerHTML = verbs[todaysVerb].pres.nosotros;
-document.getElementById('pres_uds').innerHTML = verbs[todaysVerb].pres.ustedes;
+// pt# need to import verbs and todaysVerb to satisfy linter
+const tenses = ['pres', 'imperf', 'past', 'futur'];
 
-document.getElementById('imperf_yo').innerHTML = verbs[todaysVerb].imperf.yo;
-document.getElementById('imperf_tu').innerHTML = verbs[todaysVerb].imperf.tu;
-document.getElementById('imperf_el').innerHTML = verbs[todaysVerb].imperf.el;
-document.getElementById('imperf_nos').innerHTML = verbs[todaysVerb].imperf.nosotros;
-document.getElementById('imperf_uds').innerHTML = verbs[todaysVerb].imperf.ustedes;
-
-document.getElementById('past_yo').innerHTML = verbs[todaysVerb].past.yo;
-document.getElementById('past_tu').innerHTML = verbs[todaysVerb].past.tu;
-document.getElementById('past_el').innerHTML = verbs[todaysVerb].past.el;
-document.getElementById('past_nos').innerHTML = verbs[todaysVerb].past.nosotros;
-document.getElementById('past_uds').innerHTML = verbs[todaysVerb].past.ustedes;
-
-document.getElementById('futur_yo').innerHTML = verbs[todaysVerb].futur.yo;
-document.getElementById('futur_tu').innerHTML = verbs[todaysVerb].futur.tu;
-document.getElementById('futur_el').innerHTML = verbs[todaysVerb].futur.el;
-document.getElementById('futur_nos').innerHTML = verbs[todaysVerb].futur.nosotros;
-document.getElementById('futur_uds').innerHTML = verbs[todaysVerb].futur.ustedes;
-
-document.getElementById('definition').innerHTML = verbs[todaysVerb].translation;
-
+for (let i = 0; i < tenses.length; i += 1) {
+  const tense = tenses[i];
+  const div = document.getElementById(tense);
+  const divPs = div.getElementsByTagName('p');
+  for (let j = 0; j < divPs.length; j += 1) {
+    const input = divPs[j];
+    const pId = divPs[j].id;
+    const translation = verbs[todaysVerb][tense][pId];
+    input.innerHTML = translation;
+  }
+}
